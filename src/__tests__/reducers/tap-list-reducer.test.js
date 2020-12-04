54b7ca716f8606e3932bd20f805f291d7e360672
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 08:37:14 -0800

    good fail for tap-list-reducer test 2

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -3,7 +3,40 @@ import tapListReducer from '../../reducers/tap-list-reducer';
 
 describe('tap-list-reducer', () => {
 
+  const newKeg = {
+    name: "testBeer",
+    brand: "testBrand",
+    price: "4.00",
+    alcoholContent: "6.5",
+    quantity: "10",
+    id: "10001"
+  }  
+
+  const startingList = [];
+
   test('1 Should return default state if no action is triggered', () => {    
     expect(tapListReducer(undefined, {type: null})).toEqual([])
   })
+
+  test('Should concat new keg to tap list', () => {
+    const { name, brand, price, alcoholContent, quantity, id } = newKeg;
+    const action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      quantity: quantity,
+      id: id  
+    }
+
+    expect(tapListReducer(startingList, action)).toEqual([{
+      name: "testBeer",
+      brand: "testBrand",
+      price: "4.00",
+      alcoholContent: "6.5",
+      quantity: "10",
+      id: "10001"
+    }])
+  });
 });
