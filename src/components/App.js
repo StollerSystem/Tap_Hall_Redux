7ea6c1eaae48093c6b02f20e1838189c1ec05603
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 15:04:21 -0800

    change side images

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,8 @@ function App(){
       <div className="container">
         <div className="row">
           <div className="col-2">
-            <img className="shield" src={require('./viking_PNG84.png')} alt="ship" />
-            <img className="shield" src={require('./viking_PNG84.png')} alt="ship" />
-            <img className="shield" src={require('./viking_PNG84.png')} alt="ship" />
-            <img className="shield" src={require('./viking_PNG84.png')} alt="ship" />
-            <img className="shield" src={require('./viking_PNG84.png')} alt="ship" />
+            <img className="shield" src={require('./VikingMammen2.png')} alt="ship" />
+           
             
           </div>
           <div className="col-8">
@@ -21,7 +18,7 @@ function App(){
             <TapControl />          
           </div>
           <div className="col-2">
-            
+          <img className="shield" src={require('./VikingMammen1.png')} alt="ship" />
           </div>
         </div>
       </div>
