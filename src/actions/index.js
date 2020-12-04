0af6ae253d3ea43034cc84b0491684f022d0e850
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 09:21:30 -0800

    pass Action/index test 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,12 @@
 export const addKeg = (newKeg) => {
-  
+  const { name, brand, price, alcoholContent, quantity, id } = newKeg;
+  return {    
+    type: 'ADD_KEG',
+    name: name,
+    brand: brand,
+    price: price,
+    alcoholContent: alcoholContent,
+    quantity: quantity,
+    id: id  
+  }
 }
