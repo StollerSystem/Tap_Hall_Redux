7313c30149dfca283eba54ff70a0f1e75e3f292a
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 09:07:26 -0800

    add action constants

---
+++ src/actions/ActionType.js
@@ -0,0 +1,6 @@
+export const ADD_KEG = 'ADD_KEG';
+export const DELETE_KEG = 'DELETE_KEG';
+export const POUR_PINT = 'POUR_PINT';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
+export const SELECTED_KEG = 'SELECTED KEG';
+export const EDITING = 'EDITING';
