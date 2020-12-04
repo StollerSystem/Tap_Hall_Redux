568a22f22ad71050b83dd6abcccbf83aa8d29b7c
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 08:57:52 -0800

    good fail for editing-reducer test 1

---
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -0,0 +1,10 @@
+import editingReducer from '../../reducers/editing-reducer';
+
+describe("editingReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(editingReducer(false, { type: null })).toEqual(false);
+  });
+
+  
+});
