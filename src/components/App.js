a758cefc603541bbe1c2d8e7d172ad1977121d10
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 15:14:59 -0800

    move images into folder

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,14 @@ function App(){
       <div className="container">
         <div className="row">
           <div className="col-2">
-            <img className="shield" src={require('./VikingMammen2.png')} alt="ship" />
-           
-            
+            <img className="sideImg" src={require('./img/VikingMammen2.png')} alt="ship" />            
           </div>
-          <div className="col-8">
+          <div className="col-lg-8">
             <Header />
             <TapControl />          
           </div>
           <div className="col-2">
-          <img className="shield" src={require('./VikingMammen1.png')} alt="ship" />
+          <img className="sideImg" src={require('./img/VikingMammen1.png')} alt="ship" />
           </div>
         </div>
       </div>
