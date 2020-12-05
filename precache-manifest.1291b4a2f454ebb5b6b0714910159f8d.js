2a9a8da8b8eb0cd2460845a106542626da4bd089
Ben Stoller <stollersystemsound@gmail.com>
2020-12-04 16:18:34 -0800

    Updates

---
+++ precache-manifest.1291b4a2f454ebb5b6b0714910159f8d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1890d83dda21711f4d2fcfecce1372f4",
+    "url": "/Tap_Room_Redux/index.html"
+  },
+  {
+    "revision": "718c797055ba2b32310d",
+    "url": "/Tap_Room_Redux/static/css/main.ed981ae9.chunk.css"
+  },
+  {
+    "revision": "034661e193fc99af835f",
+    "url": "/Tap_Room_Redux/static/js/2.5f0b9b8f.chunk.js"
+  },
+  {
+    "revision": "e57c0a37952a3d251c6b",
+    "url": "/Tap_Room_Redux/static/js/3.79d8dd49.chunk.js"
+  },
+  {
+    "revision": "718c797055ba2b32310d",
+    "url": "/Tap_Room_Redux/static/js/main.d5304d89.chunk.js"
+  },
+  {
+    "revision": "d82512bd57cedad78074",
+    "url": "/Tap_Room_Redux/static/js/runtime-main.06c73e8f.js"
+  },
+  {
+    "revision": "0d992982cb6d24301efdd8ce0a4bb01f",
+    "url": "/Tap_Room_Redux/static/media/ClipartKey_1391822B.0d992982.png"
+  },
+  {
+    "revision": "7bc105e1fbb2887282ed690fc672db10",
+    "url": "/Tap_Room_Redux/static/media/VikingMammen1.7bc105e1.png"
+  },
+  {
+    "revision": "55ffd52f30e64135249bc92e3e6a8566",
+    "url": "/Tap_Room_Redux/static/media/VikingMammen2.55ffd52f.png"
+  }
+]);
